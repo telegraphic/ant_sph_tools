7f90c13aedf76b871f521e3b078e2248b290ac72
Danny Price <dan@thetelegraphic.com>
2024-12-13 23:39:46 +0800

    Adding initial tests

---
+++ tests/test_read_sph.py
@@ -0,0 +1,21 @@
+from ant_sph_tools.io import read_sph
+from scipy.io import loadmat
+import numpy as np
+
+def test_read_sph():
+    """Test read_sph function.
+
+    Compares against stored MATLAB output.
+    """
+    sph_data = read_sph('dipole_FarField1_299MHz.sph')
+
+    # Load MATLAB workspace
+    mat_data = loadmat('tests/test_ReadSphModes.mat')
+    
+    assert np.allclose(sph_data['Q'], mat_data['Q'])
+    assert np.allclose(sph_data['j'], mat_data['j'])
+    assert sph_data['mmax'] == mat_data['mmax']
+    assert sph_data['nmax'] == mat_data['nmax']
+
+if __name__ == "__main__":
+    test_read_sph()
